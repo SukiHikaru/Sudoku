5a40634bc2085fbff44e660174aff448a4d8f8fb
Suki Nguyen <hongnguyen24@gmail.com>
2020-03-26 20:29:35 +0100

    create gui for this game

--- solver.py
+++ solver.py
@@ -1,3 +1,7 @@
+
+
+
+
 board = [
     [7, 8, 0, 4, 0, 0, 1, 2, 0],
     [6, 0, 0, 0, 7, 5, 0, 0, 9],
@@ -9,6 +13,24 @@ board = [
     [1, 2, 0, 0, 0, 7, 4, 0, 0],
     [0, 4, 9, 2, 0, 6, 0, 0, 7]
 ]
+
+
+def solve(bo):
+    find = find_empty(bo)
+    if not find:
+        return True  # we found the solution
+    else:
+        row, col = find
+
+    for i in range(1, 10):  # loop through value from 1 to 9
+        if valid(bo, i, (row, col)):  # check by adding into the board if the value is valid
+            bo[row][col] = i  # if it true, add into the board
+
+            if solve(bo):  # recursively try to finish the solution
+                return True
+            bo[row][col] = 0
+    return False
+
 
 def valid(bo, num, pos):
     # check row
@@ -24,18 +46,20 @@ def valid(bo, num, pos):
     box_x = pos[1] // 3
     box_y = pos[0] // 3
 
-    for i in range(box_y*3, box_y*3 +3):
+    for i in range(box_y * 3, box_y * 3 + 3):
         for j in range(box_x * 3, box_x * 3 + 3):
-            if bo[i][j] == num and (i,j) != pos:
+            if bo[i][j] == num and (i, j) != pos:
                 return False
     return True
+
+
 # print this board
 def print_board(bo):
     for i in range(len(bo)):  # Zeilen
         if i % 3 == 0 and i != 0:
             print('- - - - - - - - - - - - ')
         for j in range(len(bo[0])):
-            if j % 3 == 0  and j != 0:
+            if j % 3 == 0 and j != 0:
                 print(' | ', end='')
 
             if j == 8:
@@ -48,7 +72,11 @@ def find_empty(bo):
     for i in range(len(bo)):
         for j in range(len(bo[0])):
             if bo[i][j] == 0:
-                return (i, j) # row, col
+                return (i, j)  # row, col
+
+    return None
 
 
+print_board(board)
+solve(board)
 print_board(board)
